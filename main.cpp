ea474dbd2bac879aa1a90e419405e431084f786b
joigno <30682875+joigno@users.noreply.github.com>
2023-04-10 07:30:06 +0200

    remove buffer example in main.

--- main.cpp
+++ main.cpp
@@ -16,14 +16,5 @@ int main() {
     bool result = sloth.verifyProofVDF(t, x, y);
     std::cout << "Verification result: " << (result ? "true" : "false") << std::endl;
 
-    // Generate buffer proof
-    std::vector<uint8_t> x_buffer(16, 0);
-    sloth.writeBigUIntLE(x, x_buffer, 16);
-    std::vector<uint8_t> y_buffer = sloth.generateBufferProofVDF(t, x_buffer, 16);
-
-    // Verify buffer proof
-    bool buffer_result = sloth.verifyBufferProofVDF(t, x_buffer, y_buffer, 16);
-    std::cout << "Buffer verification result: " << (buffer_result ? "true" : "false") << std::endl;
-
     return 0;
 }
